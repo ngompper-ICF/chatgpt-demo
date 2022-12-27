f08d6f04413eae37f237b89d505d7e936fb6e9e1
Nick Gompper (ICF) <nick.gompper@icf.com>
2022-12-27 00:23:21 -0500

    add curl test

---
+++ curl_test.sh
@@ -0,0 +1,7 @@
+curl -X POST \
+  https://bbfzuxkkvf.execute-api.us-east-1.amazonaws.com/v1/sort \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "sortBy": "alphabetical",
+  "data": ["apple", "banana", "cherry"]
+}'
